1cab2f453950bc1bc18d2e1b77322a7c7382b9c7
Christoffer Klang <christoffer.klang@gmail.com>
2018-12-01 16:40:59 +0000

    Parse more files

--- parse_pdf.py
+++ parse_pdf.py
@@ -16,10 +16,17 @@ from nltk.stem import SnowballStemmer
 from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 
+KEYWORDS_WE_WANT = ['church', 'school']
+
+def get_text_lang(word_tokens):
+    """Returns the language based on the given tokens. 100% accurate and complete solution."""
+    swe_char_re = re.compile('[åäö]')
+    is_probably_swedish = any(swe_char_re.search(token) for token in word_tokens)
+    return 'swedish' if is_probably_swedish else 'english'
+
 def pdf_to_text(filename):
     resource_manager = PDFResourceManager(caching=True)
     outfile = io.StringIO()
-    # outfile = open(filename, 'w')
     la_params = LAParams()
     device = TextConverter(
         resource_manager, outfile, codec='utf-8', laparams=la_params, imagewriter=None
@@ -35,23 +42,34 @@ def pdf_to_text(filename):
     device.close()
     return outfile.getvalue()
 
-def get_word_tokens(text):
+def tokenize(text):
     tokens = nltk.word_tokenize(text)
     # Filter out tokens that aren't words, because we only care about keywords
-    word_tokens = [token for token in tokens if re.match(r'^\w+$', token)]
-    # Filter out stopwords
-    return [token for token in word_tokens if not token in stopwords.words('swedish')]
+    word_tokens = [token.lower() for token in tokens if re.match(r'^\w+$', token.lower())]
+    return word_tokens
 
+def parse_doc(filepath):
+    print("Reading %s and converting it to text..." % filepath)
+    text = pdf_to_text(filepath).strip()
+    if text == '':
+        print("Not parseable %s" % filepath)
+        return
+    
+    word_tokens = tokenize(text)
+    lang = get_text_lang(word_tokens)
+    print(" - document language %s" % lang)
+        
+    stemmer = SnowballStemmer(lang)
+    stopword_set = stopwords.words(lang)
+    normalized_tokens = [stemmer.stem(token) for token in word_tokens if not token in stopword_set]
+
+    most_common_words = [word for word, _ in FreqDist(normalized_tokens).most_common(100)]
+    print(most_common_words)
 
 def main():
-    text = pdf_to_text("sample_pdf/not_a_result_file.pdf")
-    stemmer = SnowballStemmer("swedish", ignore_stopwords=True)
-    normalized_tokens = [
-        stemmer.stem(token.lower())
-        for token in get_word_tokens(text)
-    ]
-    print(FreqDist(normalized_tokens).most_common(10))
-
+    parse_doc("sample_pdf/not_a_result_file_sv.pdf")
+    parse_doc("sample_pdf/not_a_result_file_en.pdf")
+    parse_doc("sample_pdf/result_en.pdf")
 
 if __name__ == "__main__":
     main()
